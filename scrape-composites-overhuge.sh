93ec2fbc160c328a832129d0f02b839c0af09d91
hennickc <hennickc@amazon.com>
2025-02-25 18:27:56 -0800

    Fix: scrape-composites-overhuge minimum size was smaller than scrape-composites-large maximum

--- scrape-composites-overhuge.sh
+++ scrape-composites-overhuge.sh
@@ -11,7 +11,7 @@ done
 
 while : ; do
   let "start = (job % 21) * 5000" # start must be in range 0 to 100,000
-  let "digits = ((job * 11) % 23) + 92" # 92-124 digits; too big to factor ourselves
+  let "digits = ((job * 8) % 19) + 96" # 96-124 digits; too big to factor ourselves
   url="https://factordb.com/listtype.php?t=3&mindig=${digits}&perpage=5000&start=${start}&download=1"
   sem --id 'factordb-curl' --fg -j 4 wget -e robots=off -nv --no-check-certificate -O- "$url"
   echo "Requested ${digits}-digit composites starting at ${start}."
