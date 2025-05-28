6662dafa844825c82b8f02b741a7e0e7da55e0ce
hennickc <hennickc@amazon.com>
2025-05-28 16:12:30 -0700

    Combine scrape-unknowns into scrape-stats

--- scrape-stats.sh
+++ scrape-stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -u
 get_row () {
-  pup -p "body table:nth-child(4) tr:nth-child($2) td:nth-child(2)" <<< "$1" \
+  pup -p "body table:nth-child($2) tr:nth-child($3) td:nth-child(2)" <<< "$1" \
     | sed 's_<td[^>]*>__' \
     | sed 's_</td>__' \
     | tr -d '\n' \
@@ -12,14 +12,19 @@ while true; do
   (
     results=$(sem --id 'factordb-curl' -j 4 wget -e robots=off -nv --no-check-certificate -O- -o /dev/null 'https://factordb.com/status.php')
     time=$(date -u --iso-8601=seconds)
-    p=$(get_row "${results}" 2)
-    prp=$(get_row "${results}" 3)
-    cf=$(get_row "${results}" 4)
-    c=$(get_row "${results}" 5)
-    u=$(get_row "${results}" 6)
+    p=$(get_row "${results}" 4 2)
+    prp=$(get_row "${results}" 4 3)
+    cf=$(get_row "${results}" 4 4)
+    c=$(get_row "${results}" 4 5)
+    u=$(get_row "${results}" 4 6)
     if [ "${u}" != "" ]; then
       echo "\"${time}\",${p},${prp},${cf},${c},${u}" | tee -a stats.csv
     fi
+    assign_least_u=$(get_row "${results}" 6 4 | grep -o 'index.php?id=[0-9]+' \
+      | sed 's_.*index.php_https://factordb.com/index.php_' \
+      | sed 's_$_\&prp=Assign+to+worker_')
+    result=$(sem --id 'factordb-curl' --ungroup -j 4 xargs wget -e robots=off --no-check-certificate -nv -O- <<< "${assign_least_u}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
+    echo $result
   ) &
   next_row_proc=$!
   sleep 119.5
