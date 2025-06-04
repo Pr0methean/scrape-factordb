89f28e45a0d58c00ecf985b35b210e8c8f5900e4
hennickc <hennickc@amazon.com>
2025-06-03 17:32:11 -0700

    Revert "Combine scrape-unknowns into scrape-stats"
    This reverts commit 6662dafa844825c82b8f02b741a7e0e7da55e0ce.

--- scrape-stats.sh
+++ scrape-stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -u
 get_row () {
-  pup -p "body table:nth-child($2) tr:nth-child($3) td:nth-child(2)" <<< "$1" \
+  pup -p "body table:nth-child(4) tr:nth-child($2) td:nth-child(2)" <<< "$1" \
     | sed 's_<td[^>]*>__' \
     | sed 's_</td>__' \
     | tr -d '\n' \
@@ -29,11 +29,11 @@ while true; do
   (
     results=$(sem --id 'factordb-curl' -j 4 wget -e robots=off -nv --no-check-certificate -O- -o /dev/null 'https://factordb.com/status.php')
     time=$(date -u --iso-8601=seconds)
-    p=$(get_row "${results}" 4 2)
-    prp=$(get_row "${results}" 4 3)
-    cf=$(get_row "${results}" 4 4)
-    c=$(get_row "${results}" 4 5)
-    u=$(get_row "${results}" 4 6)
+    p=$(get_row "${results}" 2)
+    prp=$(get_row "${results}" 3)
+    cf=$(get_row "${results}" 4)
+    c=$(get_row "${results}" 5)
+    u=$(get_row "${results}" 6)
     if [ "${u}" != "" ]; then
       echo "\"${time}\",${p},${prp},${cf},${c},${u}" | tee -a stats.csv
       least_u_row=$(get_row "${results}" 6 4)
