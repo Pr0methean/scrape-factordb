983e6dca63d3b17f3fb3e22889b4c09bde58d5ed
hennickc <hennickc@amazon.com>
2025-06-05 12:39:22 -0700

    Occasionally scrape tiny composites instead of overhuge

--- scrape-composites-overhuge.sh
+++ scrape-composites-overhuge.sh
@@ -11,7 +11,11 @@ done
 
 while : ; do
   let "start = ((job * 8) % 21) * 5000" # start must be in range 0 to 100,000
-  let "digits = ((job * 11) % 23) + 96" # 96-128 digits; too big to factor ourselves
+  let "digits = ((job * 11) % 23) + 96" # 96-127 digits; too big to factor ourselves
+  if [ $digits -eq 128 ]; then
+    let "start = 0"
+    let "digits = 1"
+  fi
   url="https://factordb.com/listtype.php?t=3&mindig=${digits}&perpage=5000&start=${start}&download=1"
   sem --id 'factordb-curl' --fg -j 4 wget -e robots=off -nv --no-check-certificate -O- "$url"
   echo "Requested ${digits}-digit composites starting at ${start}."
