0e60e92e889dafb8658af1a59de3e1a094b44ebc
hennickc <hennickc@amazon.com>
2025-06-05 13:00:31 -0700

    Track smallest PRP and C in stats.csv

--- scrape-stats.sh
+++ scrape-stats.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -u
 get_row () {
-  pup -p "body table:nth-child(4) tr:nth-child($2) td:nth-child(2)" <<< "$1" \
+  pup -p "body table:nth-child($2) tr:nth-child($3) td:nth-child(2)" <<< "$1" \
     | sed 's_<td[^>]*>__' \
     | sed 's_</td>__' \
     | tr -d '\n' \
-    | tr -d ','
+    | tr -d ',' \
+    | tr -d ' '
 }
 
 mkdir -p "/tmp/factordb-scraped-unknowns"
@@ -14,13 +15,19 @@ while true; do
   (
     results=$(sem --id 'factordb-curl' -j 4 wget -e robots=off -nv --no-check-certificate -O- -o /dev/null 'https://factordb.com/status.php')
     time=$(date -u --iso-8601=seconds)
-    p=$(get_row "${results}" 2)
-    prp=$(get_row "${results}" 3)
-    cf=$(get_row "${results}" 4)
-    c=$(get_row "${results}" 5)
-    u=$(get_row "${results}" 6)
+    p=$(get_row "${results}" 4 2)
+    prp=$(get_row "${results}" 4 3)
+    cf=$(get_row "${results}" 4 4)
+    c=$(get_row "${results}" 4 5)
+    u=$(get_row "${results}" 4 6)
+    smallest_prp_cell=$(get_row "${results}" 6 2)
+    # echo ${smallest_prp_cell}
+    smallest_prp=$(sed 's_digits.*__' <<< "${smallest_prp_cell}")
+    # echo "Smallest PRP size is ${smallest_prp}"
+    smallest_c_cell=$(get_row "${results}" 6 3)
+    smallest_c=$(sed 's_digits.*__' <<< "${smallest_c_cell}")
     if [ "${u}" != "" ]; then
-      echo "\"${time}\",${p},${prp},${cf},${c},${u}" | tee -a stats.csv
+      echo "\"${time}\",${p},${prp},${cf},${c},${u},${smallest_prp},${smallest_c}" | tee -a stats.csv
     fi
   ) &
   next_row_proc=$!
