7c8f706254518e6e4108257cb35fbde3156be664
hennickc <hennickc@amazon.com>
2025-06-05 13:53:21 -0700

    Add backoff to scrape-unknowns.sh

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -4,6 +4,7 @@ let "min_start = 0"
 let "max_start = 50"
 let "start = $min_start"
 let "perpage = 3"
+let "waits = 0"
 urlstart="https://factordb.com/listtype.php?t=2\&mindig="
 while true; do
 url="${urlstart}${digits}\&perpage=${perpage}\&start=${start}"
@@ -18,7 +19,16 @@ declare assign_url
 while read -r assign_url; do
     result=$(sem --id 'factordb-curl' --ungroup -j 4 xargs wget -e robots=off --no-check-certificate -nv -O- <<< "${assign_url}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
     echo $result
-    sleep 1
+    grep -q 'Assigned' <<< $result
+    if [ $? -eq 0 ]; then
+      let "waits = 0"
+    elif [ $waits -lt 4 ]; then
+        grep -q 'Please wait' <<< $result
+        if [ $? -eq 0 ]; then
+            let "waits += 1"
+        fi
+    fi
+    sleep $((2 ** ${waits}))
 done <<< "${assign_urls}"
 let "start = ($start + $perpage) % ${max_start}"
 done
