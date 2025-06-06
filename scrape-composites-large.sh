80d2dbe354f6ad1f1bdd526acb92f01830969eab
hennickc <hennickc@amazon.com>
2025-06-05 18:40:23 -0700

    Process numbers up to 88 digits in batches of 2

--- scrape-composites-large.sh
+++ scrape-composites-large.sh
@@ -10,12 +10,15 @@ while [ ! -f "${fifo_id}" ]; do
   let "digits = 96 - (($job * 5) % 11)" # Range of 86-96 digits
   if [ $digits -lt 89 ]; then
     let "start = ($job % 23) * 500"
+    let "perpage = 2"
   elif [ 13 -eq $(( (job % 43) % 23 )) ]; then
     let "start = 100000"
+    let "perpage = 1"
   else
     let "start = (($job * 91) % 200) * 500"
+    let "perpage = 1"
   fi
-  echo "digits=${digits} start=${start} perpage=1 id=${job} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
+  echo "digits=${digits} start=${start} perpage=${perpage} id=${job} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
   let "job++"
 done &
 tail -f "${fifo_id}" | parallel -j '/tmp/scrape-composites-large-threads' --ungroup
