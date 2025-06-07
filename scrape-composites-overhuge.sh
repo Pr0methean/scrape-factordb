72842d743d322d691a9292c5cd8e6d6188512ce2
hennickc <hennickc@amazon.com>
2025-06-07 14:36:44 -0700

    Let scrape-composites-overhuge attack 93-digit numbers

--- scrape-composites-overhuge.sh
+++ scrape-composites-overhuge.sh
@@ -2,8 +2,8 @@
 set -u
 job=1050000
 
-# Start at a random point in the cycle, and avoid modulo bias
-while [ $job -ge 65100 ]; do
+# Start at a random point in the cycle of 21*37, and avoid modulo bias
+while [ $job -ge 65268 ]; do
 #   myrandom=$(openssl rand 3 | od -DAn)
 #   job=$(($myrandom / 160))
   job=$(openssl rand 2 | od -DAn)
@@ -11,8 +11,8 @@ done
 
 while : ; do
   let "start = ((job * 8) % 21) * 5000" # start must be in range 0 to 100,000
-  let "digits = ((job * 13) % 31) + 94" # 94-123 digits; too big or on the borderline of what we can factor ourselves
-  if [ $digits -eq 124 ]; then
+  let "digits = ((job * 17) % 37) + 93" # 93-127 digits; too big or on the borderline of what we can factor ourselves
+  if [ $digits -gt 127 ]; then
     let "start = 0"
     let "digits = 1"
   fi
