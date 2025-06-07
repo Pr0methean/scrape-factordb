8ad82074dec79cac2edeffcab7e861379c94c765
hennickc <hennickc@amazon.com>
2025-06-07 13:32:13 -0700

    Raise minimum size to 89 digits, and start factoring-job IDs at 1

--- scrape-composites-large.sh
+++ scrape-composites-large.sh
@@ -3,22 +3,22 @@ set -u
 fifo_id="/tmp/$(uuidgen)"
 mkfifo "${fifo_id}"
 let "job = 99999999"
-while [ $job -ge "16718400" ]; do
+while [ $job -ge "16767000" ]; do
   let "job = $(openssl rand 3 | od -DAn)"
 done
+let "id = 1"
 while [ ! -f "${fifo_id}" ]; do
-  let "digits = 96 - (($job * 5) % 11)" # Range of 86-96 digits
-  if [ $digits -lt 89 ]; then
-    let "start = ($job % 23) * 500"
-    let "perpage = 2"
-  elif [ 13 -eq $(( (job % 43) % 23 )) ]; then
+  let "digits = 96 - (($job * 5) % 8)" # Range of 89-96 digits
+  let "perpage = 1"
+  if [ 13 -eq $(( (job % 43) % 23 )) ]; then
     let "start = 100000"
     let "perpage = 1"
   else
     let "start = (($job * 91) % 200) * 500"
     let "perpage = 1"
   fi
-  echo "digits=${digits} start=${start} perpage=${perpage} id=${job} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
+  echo "digits=${digits} start=${start} perpage=${perpage} id=${id} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
   let "job++"
+  let "id++"
 done &
 tail -f "${fifo_id}" | parallel -j '/tmp/scrape-composites-large-threads' --ungroup
