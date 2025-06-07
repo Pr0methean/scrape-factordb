d2def02f11a5087b33ced71feb7580f663ecda55
hennickc <hennickc@amazon.com>
2025-06-07 10:58:01 -0700

    Increase minimum delay in scrape-unknowns

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -6,7 +6,7 @@ let "start = $min_start"
 let "perpage = 3"
 let "waits = 0"
 urlstart="https://factordb.com/listtype.php?t=2\&mindig="
-delays=(2 3 4 5.5 8 11 16)
+delays=(4 5.5 8 11 16)
 let "max_waits = ${#delays[@]} - 1"
 while true; do
 url="${urlstart}${digits}\&perpage=${perpage}\&start=${start}"
