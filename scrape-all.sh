e8ab22455fbb4cc6f77b288a41c704c406d83e31
hennickc <hennickc@amazon.com>
2025-06-07 11:29:28 -0700

    Create scrape-all.sh to launch all scrapers

---
+++ scrape-all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo 1 > /tmp/scrape-composites-large-threads
+gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
+gnome-terminal --tab --title=HugeComposites -- nice -+19 ./scrape-composites-overhuge.sh 2>&1 | tee /tmp/scrape-composites-overhuge.txt
+gnome-terminal --tab --title=Unknowns -- bash -c "digits=3000 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
+gnome-terminal --tab --title=PRPs -- bash -c "digits=300 start=0 perpage=10 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
+gnome-terminal --tab --title=Factoring -- ./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt
+
