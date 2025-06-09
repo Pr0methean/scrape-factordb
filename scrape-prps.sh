4bc84d85cf56094d2b3ba727649c89ad4d857079
hennickc <hennickc@amazon.com>
2025-06-09 14:35:24 -0700

    Adjust scrape-prps request frequency based on size of prime

--- scrape-prps.sh
+++ scrape-prps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -u
-
+let "start = 0"
 urlstart='https://factordb.com/listtype.php?t=1\&mindig='
 let "bases_left_since_restart = 0"
+let "bases_per_restart = 254 * $perpage * 2"
 while true; do
 url="${urlstart}${digits}\&perpage=${perpage}\&start=${start}"
 echo "Running search: ${url}"
@@ -17,6 +18,8 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
   fi
   echo "Checking ID ${id}"
   status=$(sem --id 'factordb-curl' -j 4 --fg xargs wget -e robots=off --no-check-certificate -t 10 -nv -O- <<< "${id}\&open=prime\&ct=Proof")
+  digits=$(grep -o '&lt;[0-9]\+&gt;' <<< "$status" | head -n 1 | grep -o '[0-9]\+')
+  echo "PRP with ID ${id} is ${digits} digits."
   bases_checked_html=$(grep -A1 'Bases checked' <<< "$status")
   echo "$bases_checked_html"
   bases_checked_lines=$(grep -o '[0-9]\+' <<< "$bases_checked_html")
@@ -25,6 +28,7 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
   declare -a bases_left
   readarray -t bases_left < <(echo "${bases[@]} ${bases_checked_lines}" | tr ' ' '\n' | sort -n | uniq -u | grep .)
   let "bases_left_on_page += ${#bases_left[@]}"
+  let "bases_left_since_restart += ${bases_left_on_page}"
   urls=()
   for base in "${bases_left[@]}"; do
     urls+=("${id}\&open=prime\&basetocheck=${base}")
@@ -41,14 +45,16 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
       elif ! grep -q 'PRP' <<< "$output"; then
         echo "No longer PRP"
         break
+      else
+        sleep $((${digits} / 400))
       fi
     fi
    done
 done
-# Restart once we find and attempt 3 * 254 PRP checks that aren't already done
-if [ ${bases_left_since_restart} -ge 762 ]; then
+# Restart once we have found enough PRP checks that weren't already done
+if [ ${bases_left_since_restart} -ge ${bases_per_restart} ]; then
   start=0
-  let "bases_left_since_restart = 0"
+  bases_left_since_restart=0
 else
   start=$((($start + $perpage) % (100000 + $perpage)))
 fi
