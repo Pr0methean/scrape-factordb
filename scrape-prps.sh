bcfc3e96972c166c9c016584d0adee88ce0a19e9
hennickc <hennickc@amazon.com>
2025-06-10 16:05:06 -0700

    Keep start from increasing endlessly when search repeatedly fails

--- scrape-prps.sh
+++ scrape-prps.sh
@@ -19,7 +19,7 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
   echo "Checking ID ${id}"
   status=$(sem --id 'factordb-curl' -j 4 --fg xargs wget -e robots=off --no-check-certificate -t 10 -nv -O- --retry-connrefused --retry-on-http-error=502 <<< "${id}\&open=prime\&ct=Proof")
   digits=$(grep -o '&lt;[0-9]\+&gt;' <<< "$status" | head -n 1 | grep -o '[0-9]\+')
-  let "delay = ($digits * $digits) / 1000000"
+  let "delay = ($digits * $digits) / (800 * 800)"
   echo "PRP with ID ${id} is ${digits} digits; will wait ${delay} s between requests."
   bases_checked_html=$(grep -A1 'Bases checked' <<< "$status")
   echo "$bases_checked_html"
@@ -53,7 +53,7 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
    done
 done
 # Restart once we have found enough PRP checks that weren't already done
-if [ ${bases_left_since_restart} -ge ${bases_per_restart} ]; then
+if [ ${bases_left_since_restart} -ge ${bases_per_restart} -o ${bases_left_on_page} -eq -1 ]; then
   let "start = 0"
   let "bases_left_since_restart = 0"
 else
