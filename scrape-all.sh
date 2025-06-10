21da54217703a4336830dfffda7ce9fdc9e41b49
hennickc <hennickc@amazon.com>
2025-06-10 12:39:40 -0700

    Use pages of 3 in scrape-prps

--- scrape-all.sh
+++ scrape-all.sh
@@ -4,7 +4,7 @@
 gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
 gnome-terminal --tab --title=HugeComposites -- nice -+19 ./scrape-composites-overhuge.sh 2>&1 | tee /tmp/scrape-composites-overhuge.txt
 gnome-terminal --tab --title=Unknowns -- bash -c "digits=3000 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
-gnome-terminal --tab --title=PRPs -- bash -c "digits=300 start=0 perpage=5 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
+gnome-terminal --tab --title=PRPs -- bash -c "digits=300 start=0 perpage=3 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
 # gnome-terminal --tab --title=LargeFactoring -- ./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt
 # gnome-terminal --tab --title=MedFactoring -- ./scrape-composites-medium.sh 2>&1 | tee /tmp/scrape-composites-large.txt
 
