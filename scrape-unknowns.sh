e5c89aa66763c06448aa36a252a29f8340d343bd
hennickc <hennickc@amazon.com>
2025-06-10 10:42:58 -0700

    Restart scrape-unknowns from 0 after a search fails to load

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -2,9 +2,9 @@
 set -u
 let "start = 0"
 let "perpage = 3"
-let "delay = 6"
 let "min_delay = 1"
 let "max_delay = 45"
+let "delay = ${min_delay} * 2"
 let "delay_increment = 5"
 let "valid = 0"
 urlstart="https://factordb.com/listtype.php?t=2\&mindig="
@@ -18,7 +18,12 @@ assign_urls=$(sem --id 'factordb-curl' --ungroup -j 4 xargs wget -e robots=off -
   | sed 's_$_\&prp=Assign+to+worker_')
 declare assign_url
 let "remaining = $perpage"
+let "search_succeeded = 0"
 while read -r assign_url; do
+    if [ "${assign_url}" == "" ]; then
+      continue
+    fi
+    let "search_succeeded = 1"
     let "remaining -= 1"
     result=$(sem --id 'factordb-curl' --ungroup -j 4 xargs wget -e robots=off --no-check-certificate -nv -O- <<< "${assign_url}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
     echo $result
@@ -54,6 +59,9 @@ while read -r assign_url; do
     fi
 done <<< "${assign_urls}"
 if [ $valid -ge $(( $perpage + $perpage )) ]; then
+  let "start = 0"
+  let "valid = 0"
+elif [ ${search_succeeded} -eq 0 -a ${start} -gt 0 ]; then
   let "start = 0"
   let "valid = 0"
 else
