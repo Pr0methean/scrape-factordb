bf24e9dcf38c8e35a7c050fedd4700643cbe6a81
hennickc <hennickc@amazon.com>
2025-06-12 11:46:58 -0700

    Allow medium-size composites to start at 76 digits

--- scrape-composites-medium.sh
+++ scrape-composites-medium.sh
@@ -3,10 +3,10 @@ set -u
 fifo_id="/tmp/$(uuidgen)"
 mkfifo "${fifo_id}"
 let "job = 1"
-sizes=(32 22 16 11 8 6 4 3 2 1 1)
+sizes=(30 22 16 11 8 6 4 3 2 1 1 1 1)
 while [ ! -f "${fifo_id}" ]; do
-  let "digits = ($job % 11) + 78" # Range 78-88 digits
-  let "perpage = ${sizes[($job % 11)]}"
+  let "digits = (($job * 7) % 13) + 76" # Range 76-88 digits
+  let "perpage = ${sizes[($job % 13)]}"
   let "start = 0"
   echo "digits=${digits} start=${start} perpage=${perpage} id=${job} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
   let "job++"
