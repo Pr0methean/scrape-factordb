58fe9d8218a1d5891db55192d9139cc04ed54eca
hennickc <hennickc@amazon.com>
2025-06-13 09:05:55 -0700

    Fix: batch sizes weren't as expected

--- scrape-composites-medium.sh
+++ scrape-composites-medium.sh
@@ -3,10 +3,10 @@ set -u
 fifo_id="/tmp/$(uuidgen)"
 mkfifo "${fifo_id}"
 let "job = 1"
-sizes=(30 22 16 11 8 6 4 3 2 1 1 1 1)
+sizes=(15 11 8 6 4 3 3 2 2 1 1 1 1)
 while [ ! -f "${fifo_id}" ]; do
   let "digits = (($job * 7) % 13) + 76" # Range 76-88 digits
-  let "perpage = ${sizes[($job % 13)]}"
+  let "perpage = ${sizes[$digits - 76]}"
   let "start = 0"
   echo "digits=${digits} start=${start} perpage=${perpage} id=${job} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
   let "job++"
