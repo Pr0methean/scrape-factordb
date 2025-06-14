5c36e3d09ce9b5e3ba17b994f864953cd389fca9
hennickc <hennickc@amazon.com>
2025-06-13 18:26:38 -0700

    Redefine 'medium' as 81-88 digits

--- scrape-composites-medium.sh
+++ scrape-composites-medium.sh
@@ -5,7 +5,7 @@ mkfifo "${fifo_id}"
 let "job = 1"
 sizes=(15 11 8 6 4 3 3 2 2 1 1 1 1)
 while [ ! -f "${fifo_id}" ]; do
-  let "digits = (($job * 7) % 13) + 76" # Range 76-88 digits
+  let "digits = (($job * 5) % 8) + 81" # Range 81-88 digits
   let "perpage = ${sizes[$digits - 76]}"
   let "start = 0"
   echo "digits=${digits} start=${start} perpage=${perpage} id=${job} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
