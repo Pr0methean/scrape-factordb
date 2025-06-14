a38d34b89dc662fc85c41ab9cd827e5f2b0df9e3
hennickc <hennickc@amazon.com>
2025-06-13 18:27:20 -0700

    Don't currently need delay in scrape-prps

--- scrape-prps.sh
+++ scrape-prps.sh
@@ -18,9 +18,9 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
   fi
   echo "Checking ID ${id}"
   status=$(sem --id 'factordb-curl' -j 4 --fg xargs wget -e robots=off --no-check-certificate -t 10 -nv -O- --retry-connrefused --retry-on-http-error=502 <<< "${id}\&open=prime\&ct=Proof")
-  actual_digits=$(grep -o '&lt;[0-9]\+&gt;' <<< "$status" | head -n 1 | grep -o '[0-9]\+')
-  let "delay = ($actual_digits * $actual_digits) / 1000000"
-  echo "PRP with ID ${id} is ${actual_digits} digits; will wait ${delay} s between requests."
+#  actual_digits=$(grep -o '&lt;[0-9]\+&gt;' <<< "$status" | head -n 1 | grep -o '[0-9]\+')
+#  let "delay = ($actual_digits * $actual_digits) / 1000000"
+#  echo "PRP with ID ${id} is ${actual_digits} digits; will wait ${delay} s between requests."
   bases_checked_html=$(grep -A1 'Bases checked' <<< "$status")
   echo "$bases_checked_html"
   bases_checked_lines=$(grep -o '[0-9]\+' <<< "$bases_checked_html")
@@ -46,8 +46,8 @@ for id in $(pup 'a[href*="index.php?id"] attr{href}' <<< "$results" \
       elif ! grep -q 'PRP' <<< "$output"; then
         echo "No longer PRP"
         break
-      else
-        sleep ${delay}
+#      else
+#        sleep ${delay}
       fi
     fi
    done
