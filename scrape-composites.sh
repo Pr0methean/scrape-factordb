71c04a4adbb8c511b2bb2c32a8ab370200bc7ac0
hennickc <hennickc@amazon.com>
2025-06-20 14:30:03 -0700

    Revert "Use msieve"
    This reverts commit 7ea09f159123d898f994c70d5e766593eaeaafd4.

--- scrape-composites.sh
+++ scrape-composites.sh
@@ -36,7 +36,7 @@ mkdir -p "/tmp/factordb-composites"
           fi
           exec 9>/tmp/factordb-composites/${num}
           if flock -xn 9; then
-              echo "${id}: $(date -Is): Factoring ${num} with msieve"
+              echo "${id}: $(date -Is): Factoring ${num}"
               start_time=$(date +%s%N)
               declare factor
               while read -r factor; do
@@ -54,7 +54,7 @@ mkdir -p "/tmp/factordb-composites"
                     echo "${id}: Factor ${factor} of ${num} accepted."
                   fi
                 fi
-              done < <(msieve -e -q "${num}" | grep -o ':[0-9 ]\+' | grep -o '[0-9]\+' | head -n -1 | uniq)
+              done < <(./factor "${num}" | grep -o '[0-9]\+')
               end_time=$(date +%s%N)
               echo "${id}: $(date -Is): Done factoring ${num} after $(./format-nanos.sh $(($end_time - $start_time)))"
           else
