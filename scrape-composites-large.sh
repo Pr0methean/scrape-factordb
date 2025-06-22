5e1dee42932bbf5f1801469884432c2332a2742b
hennickc <hennickc@amazon.com>
2025-06-22 13:24:52 -0700

    Overhaul batch sizes: never more than 1 for over 100 digits

--- scrape-composites-large.sh
+++ scrape-composites-large.sh
@@ -10,7 +10,15 @@ let "id = 1"
 while [ ! -f "${fifo_id}" ]; do
   let "digits = 105 - (($job * 8) % 17)" # Range of 89-105 digits
   let "start = (($job * 91) % 210) * 500"
-  if [ $digits -le 93 -o $start -ge 100000 ]; then
+  if [ $digits -le 93 ]; then
+    if [ $start -ge 100000 ]; then
+      # Results past 100500 will only be returned as part of a page of more than 500
+      # so we switch to pages of 5000 for that size.
+      let "perpage = 20"
+    else
+      let "perpage = 2"
+    fi
+  elif [ $digits -le 100 -a $start -ge 100000 ]; then
     let "perpage = 2"
   else
     let "perpage = 1"
