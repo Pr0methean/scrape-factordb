d580c66d4018b27978eb87c1f044b6bdecb20358
hennickc <hennickc@amazon.com>
2025-06-22 13:47:02 -0700

    Factor composites of 101-117 digits on multiple threads

--- scrape-composites-huge.sh
+++ scrape-composites-huge.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -u
-fifo_id="/tmp/$(uuidgen)"
-mkfifo "${fifo_id}"
-let "job = 0"
-while [ ! -f "${fifo_id}" ]; do
-#  let "digits = 300 - ((job * 89) % 180)" # Range of 121-300 digits
-  let "digits = 122 - ((job * 13) % 31)" # Range of 92-122 digits
-  if [ 11 -eq $(( job % 23  )) ]; then
-    let "start = 100000"
-  else
-    let "start = (99501 * $RANDOM) / 32767"
-  fi
-  echo "digits=$digits start=${start} perpage=500 id=$job nice=0 ./scrape-composites.sh" >> "${fifo_id}"
+let "job = 9999999999"
+while [ $job -ge "4294967040" ]; do # Select random point in the 17*210 cycle
+  let "job = $(openssl rand 4 | od -DAn)"
+done
+let "id = 1"
+while true; do
+  let "digits = 117 - (($job * 8) % 17)" # Range of 101-117 digits
+  let "start = (($job * 91) % 210) * 500"
+  threads=$(cat /tmp/scrape-composites-huge-threads) digits=${digits} start=${start} perpage=1 id=${id} nice=0 ./scrape-composites.sh
   let "job++"
-done &
-tail -f "${fifo_id}" | parallel -j '/tmp/scrape-composites-huge-threads' --ungroup
+  let "id++"
+done
--- scrape-composites-large.sh
+++ scrape-composites-large.sh
@@ -3,19 +3,19 @@ set -u
 fifo_id="/tmp/$(uuidgen)"
 mkfifo "${fifo_id}"
 let "job = 99999999"
-while [ $job -ge "16750440" ]; do # Select random point in the 17*23*210 cycle
-  let "job = $(openssl rand 3 | od -DAn)"
+while [ $job -ge "65520" ]; do # Select random point in the 13*210 cycle
+  let "job = $(openssl rand 2 | od -DAn)"
 done
 let "id = 1"
 while [ ! -f "${fifo_id}" ]; do
-  let "digits = 105 - (($job * 8) % 17)" # Range of 89-105 digits
+  let "digits = 101 - (($job * 8) % 13)" # Range of 89-101 digits
   let "start = (($job * 91) % 210) * 500"
   if [ $digits -le 93 ]; then
     let "perpage = 2"
   else
     let "perpage = 1"
   fi
-  echo "digits=${digits} start=${start} perpage=${perpage} id=${id} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
+  echo "threads=1 digits=${digits} start=${start} perpage=${perpage} id=${id} nice=0 ./scrape-composites.sh" >> "${fifo_id}"
   let "job++"
   let "id++"
 done &
