9e43632d08e1c0a2b9eacbeafa9a0388aebd7956
hennickc <hennickc@amazon.com>
2025-06-22 14:47:46 -0700

    Scrape unknowns as small as 2001 digits

--- scrape-all.sh
+++ scrape-all.sh
@@ -2,7 +2,7 @@
 echo 11 > /tmp/scrape-composites-large-threads
 gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
 gnome-terminal --tab --title=OverHugeComposites -- nice -+19 ./scrape-composites-overhuge.sh 2>&1 | tee /tmp/scrape-composites-overhuge.txt
-gnome-terminal --tab --title=Unknowns -- bash -c "digits=3000 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
+gnome-terminal --tab --title=Unknowns -- bash -c "digits=2001 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
 gnome-terminal --tab --title=PRPs -- bash -c "digits=300 start=0 perpage=2 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
 gnome-terminal --tab --title=LargeFactoring -- bash -c "./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt"
 # gnome-terminal --tab --title=HugeFactoring -- bash -c "./scrape-composites-huge.sh 2>&1 | tee /tmp/scrape-composites-huge.txt"
