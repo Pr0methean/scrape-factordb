4c1c0e0fc9050960cfbafc924333ac1178405c2c
hennickc <hennickc@amazon.com>
2025-06-23 12:55:05 -0700

    Disable scrape-composites-tiny.sh

--- scrape-all.sh
+++ scrape-all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo 11 > /tmp/scrape-composites-large-threads
 gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
-gnome-terminal --tab --title=TinyComposites -- bash -c "nice -+19 ./scrape-composites-tiny.sh 2>&1 | tee /tmp/scrape-composites-tiny.txt"
+# gnome-terminal --tab --title=TinyComposites -- bash -c "nice -+19 ./scrape-composites-tiny.sh 2>&1 | tee /tmp/scrape-composites-tiny.txt"
 gnome-terminal --tab --title=Unknowns -- bash -c "digits=2001 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
 gnome-terminal --tab --title=PRPs -- bash -c "digits=300 start=0 perpage=2 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
 gnome-terminal --tab --title=LargeFactoring -- bash -c "./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt"
