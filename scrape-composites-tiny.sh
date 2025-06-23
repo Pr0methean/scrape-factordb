c1c8cdb365b90fe3ca7856c238c97c2a93c70f40
hennickc <hennickc@amazon.com>
2025-06-23 12:39:26 -0700

    Replace scrape-composites-overhuge.sh with scrape-composites-tiny.sh which handles only the smallest composites

---
+++ scrape-composites-tiny.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -u
+while : ; do
+  url="https://factordb.com/listtype.php?t=3&mindig=1&perpage=5000&start=0"
+  sem --id 'factordb-curl' --fg -j 4 wget --user-agent "\"Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0\"" -t 10 --retry-connrefused --retry-on-http-error=502 -e robots=off --no-check-certificate -O- "$url"
+  echo "$(date -Is): Requested tiny composites."
+  sleep 298
+done
