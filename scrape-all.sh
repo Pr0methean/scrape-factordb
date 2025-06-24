46d7c70567a83413ac3e50deb0a31291da7e0239
hennickc <hennickc@amazon.com>
2025-06-24 12:55:01 -0700

    Add scrape-composites-medium to scrape-all

--- scrape-all.sh
+++ scrape-all.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-echo 11 > /tmp/scrape-composites-large-threads
+echo 10 > /tmp/scrape-composites-large-threads
+echo 2 > /tmp/scrape-composites-medium-threads
 gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
 # gnome-terminal --tab --title=TinyComposites -- bash -c "nice -+19 ./scrape-composites-tiny.sh 2>&1 | tee /tmp/scrape-composites-tiny.txt"
-gnome-terminal --tab --title=Unknowns -- bash -c "digits=2001 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
-gnome-terminal --tab --title=PRPs -- bash -c "digits=300 start=0 perpage=2 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
-gnome-terminal --tab --title=LargeFactoring -- bash -c "./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt"
+gnome-terminal --tab --title=U -- bash -c "digits=2001 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
+gnome-terminal --tab --title=PRP -- bash -c "digits=300 start=0 perpage=2 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
+gnome-terminal --tab --title=MedC -- bash -c "./scrape-composites-medium.sh 2>&1 | tee /tmp/scrape-composites-medium.txt"
+gnome-terminal --tab --title=LargeC -- bash -c "./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt"
 # gnome-terminal --tab --title=HugeFactoring -- bash -c "./scrape-composites-huge.sh 2>&1 | tee /tmp/scrape-composites-huge.txt"
