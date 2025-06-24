52c3a7e64b7e88f607ffc68568e0988605828b98
hennickc <hennickc@amazon.com>
2025-06-24 14:45:05 -0700

    Combine medium composites into scrape-composites-large.sh

--- scrape-all.sh
+++ scrape-all.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-echo 10 > /tmp/scrape-composites-large-threads
-echo 2 > /tmp/scrape-composites-medium-threads
+echo 12 > /tmp/scrape-composites-large-threads
 gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
 # gnome-terminal --tab --title=TinyComposites -- bash -c "nice -+19 ./scrape-composites-tiny.sh 2>&1 | tee /tmp/scrape-composites-tiny.txt"
 gnome-terminal --tab --title=U -- bash -c "digits=2001 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
 gnome-terminal --tab --title=PRP -- bash -c "digits=300 start=0 perpage=2 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
-gnome-terminal --tab --title=MedC -- bash -c "./scrape-composites-medium.sh 2>&1 | tee /tmp/scrape-composites-medium.txt"
 gnome-terminal --tab --title=LargeC -- bash -c "./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt"
 # gnome-terminal --tab --title=HugeFactoring -- bash -c "./scrape-composites-huge.sh 2>&1 | tee /tmp/scrape-composites-huge.txt"
--- scrape-composites-large.sh
+++ scrape-composites-large.sh
@@ -3,14 +3,13 @@ set -u
 fifo_id="/tmp/$(uuidgen)"
 mkfifo "${fifo_id}"
 let "job = 99999999"
-while [ $job -ge "64680" ]; do # Select random point in the 11*210 cycle
+while [ $job -ge "62790" ]; do # Select random point in the 23*210 cycle
   let "job = $(openssl rand 2 | od -DAn)"
 done
 let "id = 1"
 let "minute_ns = 60 * 1000 * 1000 * 1000"
 let "hour_ns = 60 * ${minute_ns}"
 while [ ! -f "${fifo_id}" ]; do
-  let "start = (($job * 91) % 210) * 500"
   let "now = $(date +%s%N)"
   let "day_start = (${now} / (24 * ${hour_ns})) * (24 * ${hour_ns})"
   let "now_ns_of_day = ${now} - ${day_start}"
@@ -19,13 +18,18 @@ while [ ! -f "${fifo_id}" ]; do
     # when starting between 02:00 and 16:00 UTC (18:00 and 08:00 PST), softmax extends until 16:00 UTC
     let "min_softmax_ns = 16 * ${hour_ns} - ${now_ns_of_day}"
     echo "Using min_softmax_ns of ${min_softmax_ns} due to nighttime"
-    let "extra_digits = 3"
+    let "extra_digits = 10"
   else
     let "min_softmax_ns = 0"
     let "extra_digits = 0"
   fi
-  let "digits = 89 + (($job * 4) % 11) + ${extra_digits}" # Range of 89-99 digits during day, 92-102 at night
+  let "digits = 70 + (($job * 11) % 23) + ${extra_digits}" # Range of 70-92 digits during day, 80-102 at night
   let "softmax_ns = (150 - ${digits}) * ${minute_ns}"
+  if [ $digits -ge 89 ]; then
+    let "start = (($job * 91) % 210) * 500"
+  else
+    let "start = 0"
+  fi
   if [ ${softmax_ns} -lt ${min_softmax_ns} ]; then
     let "softmax_ns = ${min_softmax_ns}"
   fi
