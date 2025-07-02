519e3faddbe81edab81f66cc8578771aa3e51474
hennickc <hennickc@amazon.com>
2025-07-02 15:51:28 -0700

    Tweak scrape-unknowns.sh parameters to increase throughput

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -u
 let "start = 0"
-let "perpage = 6"
+let "perpage = 18"
+let "pertask = 3"
 let "min_delay = 1"
 let "max_delay = 45"
 let "delay = ${min_delay} * 2"
@@ -17,30 +18,30 @@ assign_urls=$(sem --id 'factordb-curl' -j 4 xargs wget -e robots=off --no-check-
   | sed 's_.\+_https://factordb.com/&\&prp=Assign+to+worker_')
 let "remaining = $perpage"
 let "search_succeeded = 0"
-results=$(sem --id 'factordb-curl' -j 4 xargs -n 2 wget -e robots=off --no-check-certificate -nv -O- -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 <<< "${assign_urls}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
+results=$(sem --id 'factordb-curl' -j 4 xargs -n ${pertask} wget -e robots=off --no-check-certificate -nv -O- -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 <<< "${assign_urls}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
     echo $results
-    grep -q 'Please wait' <<< $results
+    grep -q 'Assigned' <<< $results
     if [ $? -eq 0 ]; then
-      let "valid = 1"
-      if [ $delay -lt $delay_increment ]; then
-        let "delay *= 2"
-      else
-        let "delay += $delay_increment"
-        if [ $delay -gt $max_delay ]; then
-          let "delay = $max_delay"
-        fi
+      let "valid += 1"
+      # The following must round down to $min_delay from $min_delay + 1
+      let "delay = (17 * $delay + 5) / 20"
+      if [ $delay -lt $min_delay ]; then
+        let "delay = $min_delay"
       fi
-      # adjust for the extra delay of loading more search results
-      sleep $(($delay - 1))
     else
-      grep -q 'Assigned' <<< $results
+      grep -q 'Please wait' <<< $results
       if [ $? -eq 0 ]; then
-        let "valid += 1"
-        # The following must round down to $min_delay from $min_delay + 1
-        let "delay = (17 * $delay + 5) / 20"
-        if [ $delay -lt $min_delay ]; then
-          let "delay = $min_delay"
+        let "valid = 1"
+        if [ $delay -lt $delay_increment ]; then
+          let "delay *= 2"
+        else
+          let "delay += $delay_increment"
+          if [ $delay -gt $max_delay ]; then
+            let "delay = $max_delay"
+          fi
         fi
+        # adjust for the extra delay of loading more search results
+        sleep $(($delay - 1))
       fi
     fi
 if [ $valid -ge 3 ]; then
