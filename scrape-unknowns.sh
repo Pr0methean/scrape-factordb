dec4b8d525961f38e5c07fa8fffc69e90032e4c6
hennickc <hennickc@amazon.com>
2025-07-04 17:47:33 -0700

    Try multiple wget's per search, but in sequence

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -u
 let "start = 0"
-let "perpage = 3"
+let "perpage = 48"
 let "pertask = 3"
 let "min_delay = 1"
 let "max_delay = 45"
@@ -11,16 +11,28 @@ let "valid = 0"
 urlstart="https://factordb.com/listtype.php?t=2\&mindig="
 while true; do
 url="${urlstart}${digits}\&perpage=${perpage}\&start=${start}"
-assign_urls=$(sem --id 'factordb-curl' -j 4 xargs wget -e robots=off --no-check-certificate --retry-connrefused --retry-on-http-error=502 -T 30 -t 3 -nv -O- <<< "${url}" \
+assign_urls=$(sem --fg --id 'factordb-curl' -j 4 wget -e robots=off --no-check-certificate --retry-connrefused --retry-on-http-error=502 -T 30 -t 3 -q -O- -- "${url}" \
   | grep -o 'index.php?id=[0-9]\+' \
   | uniq \
   | tac \
   | sed 's_.\+_https://factordb.com/&\&prp=Assign+to+worker_')
+IFS=$'\n'
+assign_urls=($assign_urls)
+IFS=' '
+echo "$(date -Iseconds): Got ${#assign_urls[@]} URLs"
 let "remaining = $perpage"
 let "search_succeeded = 0"
-results=$(sem --id 'factordb-curl' -j 4 xargs -n ${pertask} wget -e robots=off --no-check-certificate -nv -O- -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 <<< "${assign_urls}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
+all_results=''
+for (( i=0; i<$perpage; i+=$pertask )); do
+    let "search_succeeded = 1"
+    # Extract a chunk using array slicing
+    chunk=("${assign_urls[@]:i:pertask}")
+    echo "${chunk[@]}"
+    results=$(sem --fg --id 'factordb-curl' -j 4 xargs wget -e robots=off --no-check-certificate -q -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 -O- <<< "${chunk[@]}" | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
     echo $results
-    grep -q 'Assigned' <<< $results
+    all_results="$all_results // $results"
+done
+grep -q 'Assigned' <<< $all_results
     if [ $? -eq 0 ]; then
       let "valid += 1"
       # The following must round down to $min_delay from $min_delay + 1
@@ -29,7 +41,7 @@ results=$(sem --id 'factordb-curl' -j 4 xargs -n ${pertask} wget -e robots=off -
         let "delay = $min_delay"
       fi
     else
-      grep -q 'Please wait' <<< $results
+      grep -q 'Please wait' <<< $all_results
       if [ $? -eq 0 ]; then
         let "valid = 1"
         if [ $delay -lt $delay_increment ]; then
