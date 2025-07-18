44770fe1400193451b901437ecfeea61a2418132
hennickc <hennickc@amazon.com>
2025-07-18 09:57:02 -0700

    scrape-all: Process only one PRP at a time, to stay within page-requests-per-hour limit

--- scrape-all.sh
+++ scrape-all.sh
@@ -2,7 +2,7 @@
 gnome-terminal --tab --title=Stats -- nice -+19 ./scrape-stats.sh
 # gnome-terminal --tab --title=TinyComposites -- bash -c "nice -+19 ./scrape-composites-tiny.sh 2>&1 | tee /tmp/scrape-composites-tiny.txt"
 gnome-terminal --tab --title=U -- bash -c "digits=2001 nice -+19 ./scrape-unknowns.sh 2>&1 | tee /tmp/scrape-unknowns.txt"
-gnome-terminal --tab --title=PRP -- bash -c "digits=300 start=0 perpage=4 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
+gnome-terminal --tab --title=PRP -- bash -c "digits=300 start=0 perpage=1 nice -+19 ./scrape-prps.sh 2>&1 | tee /tmp/scrape-prps.txt"
 echo 5 > /tmp/scrape-composites-large-threads
 gnome-terminal --tab --title=LargeC -- bash -c "./scrape-composites-large.sh 2>&1 | tee /tmp/scrape-composites-large.txt"
 # gnome-terminal --tab --title=HugeFactoring -- bash -c "./scrape-composites-huge.sh 2>&1 | tee /tmp/scrape-composites-huge.txt"
