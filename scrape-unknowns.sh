40cac4c4d342ba2cae37f3c31f0861c723ba9976
hennickc <hennickc@amazon.com>
2025-07-19 15:37:20 -0700

    Scrape-unknowns: reuse search if 'Please wait' received and no assignments made

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -7,31 +7,36 @@ urlstart="https://factordb.com/listtype.php?t=2\&mindig="
 while true; do
 url="${urlstart}${digits}\&perpage=${perpage}\&start=${start}"
 echo "$(date -Iseconds): searching ${url}"
-all_results=$(sem --fg --id 'factordb-curl' -j 4 wget -e robots=off --no-check-certificate --retry-connrefused --retry-on-http-error=502 -T 30 -t 3 -q -O- -- "${url}" \
+urls=$(sem --fg --id 'factordb-curl' -j 4 wget -e robots=off --no-check-certificate --retry-connrefused --retry-on-http-error=502 -T 30 -t 3 -q -O- -- "${url}" \
   | grep '#BB0000' \
   | grep -o 'index.php?id=[0-9]\+' \
   | uniq \
   | tac \
-  | sed 's_.\+_https://factordb.com/&\&prp=Assign+to+worker_' \
-  | sem --fg --id 'factordb-curl' -j 4 xargs -n 3 wget -e robots=off --no-check-certificate -q -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 -O- \
+  | sed 's_.\+_https://factordb.com/&\&prp=Assign+to+worker_')
+while true; do
+all_results=$(sem --fg --id 'factordb-curl' -j 4 xargs -n 3 wget -e robots=off --no-check-certificate -q -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 -O- <<< "${urls}" \
   | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
 echo "$all_results"
 assigned=$(grep -c 'Assigned' <<< $all_results)
 please_waits=$(grep -c 'Please wait' <<< $all_results)
 if [ $please_waits -gt 0 ]; then
   if [ $assigned -eq 0 ]; then
-    echo "No assignments made; waiting 10 seconds before retrying"
+    echo "No assignments made; waiting 10 seconds before retrying same search"
     sleep 10
   elif [ $please_waits -ge $assigned ]; then
     echo "Too few assignments made; waiting 7 seconds before retrying"
     sleep 7
+    break
   else
     echo "'Please wait' received; waiting 4 seconds before retrying"
     sleep 4
+    break
   fi
 else
   sleep 2
+  break
 fi
+done
 let "total_assigned += $assigned"
 let "old_start = $start"
 already_queued=$(grep -c 'queue' <<< $all_results)
