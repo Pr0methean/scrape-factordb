f5a1bf05dc44bf3805d0949dceeb18db8c337fd4
hennickc <hennickc@amazon.com>
2025-08-04 17:51:45 -0700

    scrape-unknowns: reduce 'please wait' delay to 10s; delete unused vars

--- scrape-unknowns.sh
+++ scrape-unknowns.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -u
-let "start = 0"
-let "perpage = 1"
-let "min_perpage = 1"
-let "max_perpage = 3"
-let "total_assigned = 0"
-let "minute_ns = 60 * 1000 * 1000 * 1000"
-let "min_restart = 10"
-let "max_delay = 30"
-let "min_delay = 2"
-let "delay = 20"
 urlstart="https://factordb.com/listtype.php?t=2\&mindig="
 let "entries = 0"
 while IFS=, read -r id expr; do
@@ -18,14 +8,14 @@ while true; do
   result=$(sem --fg --id 'factordb-curl' -j 2 xargs -n 3 wget -e robots=off --no-check-certificate -q -T 30 -t 3 --retry-connrefused --retry-on-http-error=502 -O- -o/dev/null -- <<< "${url}" \
     | grep '\(ssign\|queue\|>C<\|>P<\|>PRP<\)')
   echo $result
-  if [ $result == "" ]; then
+  if [ -z "$result" ]; then
     echo "Got no response for $id"
     sleep 30
   else
     grep -q 'Please wait' <<< $result
     if [ $? -eq 0 ]; then
       echo "Got 'Please wait' for $id"
-      sleep 30
+      sleep 10
     else
       break
     fi
