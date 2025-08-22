f0c5bea99b11dccc745b4874fa7fef84f1de4c11
hennickc <hennickc@amazon.com>
2025-08-22 16:19:54 -0700

    scrape-composites-server-side: Fix: need `-Ss` to minimize non-error logging

--- scrape-composites-server-side.sh
+++ scrape-composites-server-side.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while IFS=, read -r id value; do
   echo -n "${id} (${value}): "
-  curl -S --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}" \
+  curl -Ss --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}" \
     | grep "${id}" \
     | grep -o '<input type="submit"[^>]*>'
 done < <(shuf < "${source}")
