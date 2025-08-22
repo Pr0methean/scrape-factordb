3ba30f935e91971050009cfb723f9db930d5722e
hennickc <hennickc@amazon.com>
2025-08-22 16:53:47 -0700

    Fix? Log status of server-side factorization task

--- scrape-composites-server-side.sh
+++ scrape-composites-server-side.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 while IFS=, read -r id value; do
-  echo -n "${id} (${value}): "
   curl -Ss --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}"
   if [ $? -eq 0 ]; then
     echo "Checked ID ${id} (${value})"
