1b0563ce497fc37f30649259e72d5e0c0650fe3d
hennickc <hennickc@amazon.com>
2025-08-22 13:29:56 -0700

    ci: Fix typo

--- scrape-composites-server-side.sh
+++ scrape-composites-server-side.sh
@@ -1,4 +1,4 @@
-/#!/bin/bash
+#!/bin/bash
 while IFS=, read -r id value; do
   curl "https://factordb.com/sequences.php?check=${id}&fr=0&to=100&action=last20&aq=${value}" | grep submit
 done < <(shuf < "${source}")
