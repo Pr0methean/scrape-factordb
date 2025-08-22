4730429868d50703ed8fc96b7f9933a8572841af
hennickc <hennickc@amazon.com>
2025-08-22 16:17:28 -0700

    scrape-composites-server-side: Eliminate unneeded query parameters to reduce CPU load

--- scrape-composites-server-side.sh
+++ scrape-composites-server-side.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while IFS=, read -r id value; do
   echo -n "${id} (${value}): "
-  curl -S --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}&fr=0&to=100&action=last20&aq=${value}" \
+  curl -S --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}" \
     | grep "${id}" \
     | grep -o '<input type="submit"[^>]*>'
 done < <(shuf < "${source}")
