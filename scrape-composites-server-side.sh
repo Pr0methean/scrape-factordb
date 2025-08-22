fba6a08375c8138e29532cdbe698c0b9a5ce4bda
hennickc <hennickc@amazon.com>
2025-08-22 16:29:40 -0700

    Update logging for lack of superfluous parameters

--- scrape-composites-server-side.sh
+++ scrape-composites-server-side.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 while IFS=, read -r id value; do
   echo -n "${id} (${value}): "
-  curl -Ss --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}" \
-    | grep "${id}" \
-    | grep -o '<input type="submit"[^>]*>'
+  curl -Ss --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}"
+  if [ $? -eq 0 ]; then
+    echo "Checked ID ${id} (${value})"
+  else
+    echo "ERROR: Got exit status $? checking ID ${id} (${value})"
+  fi
 done < <(shuf < "${source}")
