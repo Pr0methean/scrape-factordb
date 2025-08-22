8171ee217d57fcfffe647e9b8ea43e8635b12c0c
hennickc <hennickc@amazon.com>
2025-08-22 14:02:06 -0700

    Further reduce log spam

--- scrape-composites-server-side.sh
+++ scrape-composites-server-side.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 while IFS=, read -r id value; do
-  curl -S --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}&fr=0&to=100&action=last20&aq=${value}" | grep "${id}"
+  curl -S --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}&fr=0&to=100&action=last20&aq=${value}" \
+    | grep "${id}" \
+    | grep -o '<input type="submit"[^>]*>'
 done < <(shuf < "${source}")
