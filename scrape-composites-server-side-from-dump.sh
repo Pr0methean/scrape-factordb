158e6911b31102e1c18b5a5c4d3318ee7a9f4200
hennickc <hennickc@amazon.com>
2025-08-23 10:47:20 -0700

    ci: Use dump once all search results are processed

---
+++ scrape-composites-server-side-from-dump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while IFS=, read -r id value; do
+  curl -Ss --retry 10 --retry-all-errors "https://factordb.com/sequences.php?check=${id}" >/dev/null
+  if [ $? -eq 0 ]; then
+    echo "Checked ID ${id} (${value})"
+  else
+    echo "ERROR: Got exit status $? checking ID ${id} (${value})"
+  fi
+done < <(shuf < "${source}")
